ef3441d9c9fa9ee4c5e56d0bd5685574c0ccc688
srabonti deb <69215107+Srabontideb@users.noreply.github.com>
2025-11-14 01:14:12 +0600

    Replace import for CharacterTextSplitter module

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.memory import ConversationBufferMemory
